979020f0ad31d3f52f6383a6c254c6446781b726
nhatanh <awmtrg21@gmail.com>
2020-07-23 00:00:28 +0700

    update app.py: decode base64 and save to wav file update index.html: set interval each 3 seconds add test.wav: test audio record result

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from paths import ROOT_DIR
 import wave
+import base64
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -18,10 +19,14 @@ def index():
 
 @socketio.on('catch-frame')
 def catch_frame(data):
-    print('receive: ' + data)
+    
+    b64str = data.split(',')[1]
 
     ## getting the data frames
-
+    decode = base64.b64decode(b64str)
+    
+    wav_file = open("test.wav", "wb")
+    wav_file.write(decode)
     ## do some processing
 
     ## send it back to client
